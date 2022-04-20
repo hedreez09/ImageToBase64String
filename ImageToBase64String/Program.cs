4e3fa280c353b0cc44ca6eb6ed6852c76b383ae2
Idris Wonuola <Idris.wonuola@wemabank.com>
2022-04-20 13:49:18 +0100

    Add project files.

---
+++ ImageToBase64String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ImageToBase64String
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string imagePath = @"C:\Projects\factory.png";
+			string imgBase64String = GetBase64StringForImage(imagePath);
+			Console.WriteLine(imgBase64String);
+		}
+		protected static string GetBase64StringForImage(string imgPath)
+		{
+			byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+			string base64String = Convert.ToBase64String(imageBytes);
+			return base64String;
+		}
+	}
+}
